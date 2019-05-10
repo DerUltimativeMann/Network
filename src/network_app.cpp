68408ec4f8594b7bf7250fbfc7d4b36583963a04
DerUltimativeMann <sebastian.neubert812@gmail.com>
2019-05-10 17:01:36 +0200

    basis updated

--- src/network_app.cpp
+++ src/network_app.cpp
@@ -2,6 +2,18 @@
 
 LRESULT CALLBACK network_proc_main(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     switch(msg) {
+        case WM_PAINT:
+            {
+                PAINTSTRUCT ps;
+                HDC hdc = BeginPaint(hwnd, &ps);
+
+                Block block(20, 20, 200, 150);
+
+                Rectangle(hdc, block.getX(), block.getY(), block.getW(), block.getH());
+
+                EndPaint(hwnd, &ps);
+            }
+            return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
